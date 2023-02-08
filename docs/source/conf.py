5867ffb0f455502b52d3b7512d08522cd458bbf8
SalemBajjali <94660810+SalemBajjali@users.noreply.github.com>
2023-02-08 11:25:30 -0600

    Removed image for the theme in conf.py
    Deleted html_logo = 'images/GA-logo.png'
    this goes under html_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -73,7 +73,7 @@ todo_emit_warnings = True
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-html_logo = 'images/GA-logo.png'
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
